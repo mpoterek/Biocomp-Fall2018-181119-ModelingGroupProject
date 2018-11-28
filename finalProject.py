6572fa36149c03ede5b618a9e9a9a0f56bfc54f5
Patricia Portmann <pportman@nd.edu>
2018-11-28 10:56:33 -0500

    The lotka-volterra model case 1 works with the suggested parameters.

--- finalProject.py
+++ finalProject.py
@@ -16,20 +16,20 @@ from plotnine import *
 # Challenge - Lotka Volterra
 # Assumption 1: only limiting factor for prey population growth is the predator
 # Assumption 2: the only limiting factor for predator pop growth is the availibility of prey
-def LVSim(y,t0,H,P,b,a,e,s):
-    N1=y[0]
-    N2=y[1]
-    dHdt=H*(b - a*P)*N1
-    dPdt=P*(e*a*H - s)*N2
-    
+def LVSim(y,t0,b,a,e,s):
+    H=y[0]
+    P=y[1]
+    dHdt=H*b - H*a*P
+    dPdt=P*e*a*H - P*s
+    #print(dHdt)
     
     return [dHdt,dPdt]
 
 # Case 1
-times=range(1,50)
-y0=[0.1,0.1]
-parameters=(25,5,0.5,0.02,0.1,0.2)
+times=range(1,100)
+y0=[25.,5.]
+parameters=(0.5,0.02,0.1,0.2)
 sim=spint.odeint(func=LVSim,y0=y0,t=times,args=parameters)
-#simDF=pandas.DataFrame({"t":times,"prey":sim[:,0],"predator":sim[:,1]})
-#print(ggplot(simDF,aes(x="t",y="prey"))+geom_line()+geom_line(simDF,aes(x="t",y="predator"),color="red")+theme_classic())
+simDF=pandas.DataFrame({"t":times,"prey":sim[:,0],"predator":sim[:,1]})
+print(ggplot(simDF,aes(x="t",y="prey"))+geom_line()+geom_line(simDF,aes(x="t",y="predator"),color="red")+theme_classic())
 
