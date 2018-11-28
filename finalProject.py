f0397182d2c436c296fb2899832ca7d5ba9089a1
Patricia Portmann <pportman@nd.edu>
2018-11-27 22:52:26 -0500

    inserted code for lotka-volterra from exercise 10

---
+++ finalProject.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 11:15:21 2018
+
+Look at Exercise 10 part 2 to find a basis for the Lotka-Volterra model
+
+@author: Patricia
+"""
+
+# load packages
+import pandas
+import scipy
+import scipy.integrate as spint
+from plotnine import *
+
+
